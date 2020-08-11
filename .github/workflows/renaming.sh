392c9c9dfe4d8ca5e6430edfc8a29d04419c95bc
samo_lego <34912839+samolego@users.noreply.github.com>
2020-08-11 10:49:35 +0200

    fixing name parsing

--- .github/workflows/renaming.sh
+++ .github/workflows/renaming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setting the variables
-projectId="$(grep 'archives_base_name'  | sed 's/archives_base_name = //g;s/ //g;s/,//g;s/"//g')"
+projectId="$(grep 'archives_base_name' gradle.properties | sed 's/archives_base_name = //g;s/ //g;s/,//g;s/"//g')"
 version="$(grep 'mod_version' gradle.properties | grep -o '[0-9]*\.[0-9]*\.[0-9]*')"
 mcVersion="$(grep 'minecraft_version' gradle.properties | sed 's/minecraft_version=//g;s/ //g;s/,//g;s/"//g')"
 

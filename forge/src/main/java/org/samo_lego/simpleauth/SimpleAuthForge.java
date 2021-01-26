37d4f207e2f3e6c5d15d7e4ca89ce59abf8e7d36
samolego <34912839+samolego@users.noreply.github.com>
2021-01-26 14:18:05 +0100

    Finishing Forge touches

--- forge/src/main/java/org/samo_lego/simpleauth/SimpleAuthForge.java
+++ forge/src/main/java/org/samo_lego/simpleauth/SimpleAuthForge.java
@@ -1,22 +1,18 @@
 package org.samo_lego.simpleauth;
 
 import com.mojang.brigadier.CommandDispatcher;
-import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.samo_lego.simpleauth.commands.*;
 
 @Mod(SimpleAuth.MOD_ID)
 public class SimpleAuthForge {
     public SimpleAuthForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        //EventBuses.registerModEventBus(SimpleAuth.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         SimpleAuth.init(FMLPaths.GAMEDIR.get());
         MinecraftForge.EVENT_BUS.register(this);
     }

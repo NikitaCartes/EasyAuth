5b9fb5768b08943e29fee05ed274fbbf6e91c7ed
NikitaCartes <nikich98@yandex.ru>
2021-06-24 15:57:58 +0300

    Fix forceOfflineUuid

--- common/src/main/java/org/samo_lego/simpleauth/mixin/ServerStatHandlerAccessor.java
+++ common/src/main/java/org/samo_lego/simpleauth/mixin/ServerStatHandlerAccessor.java
@@ -2,6 +2,7 @@ package org.samo_lego.simpleauth.mixin;
 
 import net.minecraft.stat.ServerStatHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.io.File;
@@ -12,6 +13,6 @@ public interface ServerStatHandlerAccessor {
     @Accessor("file")
     File getFile();
 
-    @Accessor("file")
+    @Accessor("file") @Mutable
     void setFile(File file);
 }

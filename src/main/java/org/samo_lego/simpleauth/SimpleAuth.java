3e2e046919d8bc6534d04d04a0b51bef1f27bdf2
NikitaCartes <nikich98@yandex.ru>
2021-07-11 03:11:13 +0300

    Fix 1.17.1 and version bump

--- src/main/java/org/samo_lego/simpleauth/SimpleAuth.java
+++ src/main/java/org/samo_lego/simpleauth/SimpleAuth.java
@@ -1,5 +1,13 @@
 package org.samo_lego.simpleauth;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.player.*;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
+import org.samo_lego.simpleauth.commands.*;
+import org.samo_lego.simpleauth.event.AuthEventHandler;
 import org.samo_lego.simpleauth.storage.AuthConfig;
 import org.samo_lego.simpleauth.storage.DBHelper;
 import org.samo_lego.simpleauth.storage.PlayerCache;
@@ -18,7 +26,7 @@ import java.util.concurrent.TimeUnit;
 import static org.samo_lego.simpleauth.utils.SimpleLogger.logError;
 import static org.samo_lego.simpleauth.utils.SimpleLogger.logInfo;
 
-public class SimpleAuth {
+public class SimpleAuth implements ModInitializer {
     public static final String MOD_ID = "simpleauth";
 
 
@@ -53,7 +61,7 @@ public class SimpleAuth {
 
     public static void init(Path gameDir) {
         gameDirectory = gameDir;
-        logInfo("SimpleAuth mod by samo_lego.");
+        logInfo("SimpleAuth mod by samo_lego, NikitaCartes.");
         // The support on discord was great! I really appreciate your help.
         logInfo("This mod wouldn't exist without the awesome Fabric Community. TYSM guys!");
 
@@ -93,5 +101,32 @@ public class SimpleAuth {
 
         // Closing DB connection
         DB.close();
+    }
+
+    @Override
+    public void onInitialize() {
+        SimpleAuth.init(FabricLoader.getInstance().getGameDir());
+
+        // Registering the commands
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            RegisterCommand.registerCommand(dispatcher);
+            LoginCommand.registerCommand(dispatcher);
+            LogoutCommand.registerCommand(dispatcher);
+            AuthCommand.registerCommand(dispatcher);
+            AccountCommand.registerCommand(dispatcher);
+        });
+
+        // From Fabric API
+        PlayerBlockBreakEvents.BEFORE.register((world, player, blockPos, blockState, blockEntity) -> AuthEventHandler.onBreakBlock(player));
+        UseBlockCallback.EVENT.register((player, world, hand, blockHitResult) -> AuthEventHandler.onUseBlock(player));
+        UseItemCallback.EVENT.register((player, world, hand) -> AuthEventHandler.onUseItem(player));
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, entityHitResult) -> AuthEventHandler.onAttackEntity(player));
+        UseEntityCallback.EVENT.register((player, world, hand, entity, entityHitResult) -> AuthEventHandler.onUseEntity(player));
+        ServerLifecycleEvents.START_DATA_PACK_RELOAD.register((server, serverResourceManager) -> AuthCommand.reloadConfig(null));
+        ServerLifecycleEvents.SERVER_STOPPED.register(this::onStopServer);
+    }
+
+    private void onStopServer(MinecraftServer server) {
+        SimpleAuth.stop();
     }
 }

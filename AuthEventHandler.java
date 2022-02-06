c387ad2e79e74759f3d02672a3c51e254eec8108
were491 <34976611+were491@users.noreply.github.com>
2022-02-06 00:56:45 -0600

    Add files via upload

---
+++ AuthEventHandler.java
@@ -0,0 +1,246 @@
+package xyz.nikitacartes.easyauth.event;
+
+import static xyz.nikitacartes.easyauth.EasyAuth.config;
+import static xyz.nikitacartes.easyauth.EasyAuth.playerCacheMap;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.mojang.authlib.GameProfile;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import xyz.nikitacartes.easyauth.storage.PlayerCache;
+import xyz.nikitacartes.easyauth.utils.PlayerAuth;
+import xyz.nikitacartes.easyauth.utils.TranslationHelper;
+
+/**
+ * This class will take care of actions players try to do,
+ * and cancel them if they aren't authenticated
+ */
+public class AuthEventHandler {
+
+    public static long lastAcceptedPacket = 0;
+    
+    /**
+     * Player pre-join.
+     * Returns text as a reason for disconnect or null to pass
+     *
+     * @param profile GameProfile of the player
+     * @param manager PlayerManager
+     * @return Text if player should be disconnected
+     */
+    public static LiteralText checkCanPlayerJoinServer(GameProfile profile, PlayerManager manager) {
+        // Getting the player
+        String incomingPlayerUsername = profile.getName();
+        PlayerEntity onlinePlayer = manager.getPlayer(incomingPlayerUsername);
+
+        // Checking if player username is valid
+        String regex = config.main.usernameRegex;
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(incomingPlayerUsername);
+
+        if ((onlinePlayer != null && !((PlayerAuth) onlinePlayer).canSkipAuth()) && config.experimental.preventAnotherLocationKick) {
+            // Player needs to be kicked, since there's already a player with that name
+            // playing on the server
+            return new LiteralText(
+                    String.format(
+                            config.lang.playerAlreadyOnline, onlinePlayer.getName().asString()
+                    )
+            );
+        } else if (!matcher.matches()) {
+            return new LiteralText(
+                    String.format(
+                            config.lang.disallowedUsername, regex
+                    )
+            );
+        }
+        return null;
+    }
+
+
+    // Player joining the server
+    public static void onPlayerJoin(ServerPlayerEntity player) {
+        if (((PlayerAuth) player).canSkipAuth()) {
+            player.setInvulnerable(false);
+            player.setInvisible(false);
+            return;
+        }
+        
+        // Checking if session is still valid
+        String uuid = ((PlayerAuth) player).getFakeUuid();
+        PlayerCache playerCache;
+
+        if (!playerCacheMap.containsKey(uuid)) {
+            // First join
+            playerCache = PlayerCache.fromJson(player, uuid);
+            playerCacheMap.put(uuid, playerCache);
+        } else {
+            playerCache = playerCacheMap.get(uuid);
+        }
+        if (
+                playerCache.isAuthenticated &&
+                        playerCache.validUntil >= System.currentTimeMillis() &&
+                        player.getIp().equals(playerCache.lastIp)
+        ) {
+            // Valid session
+            player.setInvulnerable(false);
+            player.setInvisible(false);
+            return;
+        }
+        
+        // If the player has too many login attempts, kick the player immediately.
+        if (playerCache.loginTries > config.main.maxLoginTries && config.main.maxLoginTries != -1) {
+            player.networkHandler.disconnect(TranslationHelper.getLoginTriesExceeded());
+            return;
+        }
+        
+        ((PlayerAuth) player).setAuthenticated(false);
+
+
+        // Tries to rescue player from nether portal
+        if (config.main.tryPortalRescue && player.getBlockStateAtPos().getBlock().equals(Blocks.NETHER_PORTAL)) {
+            BlockPos pos = player.getBlockPos();
+
+            // Teleporting player to the middle of the block
+            player.teleport(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+
+            // Faking portal blocks to be air
+            BlockUpdateS2CPacket feetPacket = new BlockUpdateS2CPacket(pos, Blocks.AIR.getDefaultState());
+            player.networkHandler.sendPacket(feetPacket);
+
+            BlockUpdateS2CPacket headPacket = new BlockUpdateS2CPacket(pos.up(), Blocks.AIR.getDefaultState());
+            player.networkHandler.sendPacket(headPacket);
+        }
+    }
+
+    public static void onPlayerLeave(ServerPlayerEntity player) {
+        if (((PlayerAuth) player).canSkipAuth())
+            return;
+        String uuid = ((PlayerAuth) player).getFakeUuid();
+        PlayerCache playerCache = playerCacheMap.get(uuid);
+        
+        if (playerCache != null && playerCache.isAuthenticated) {
+            playerCache.lastIp = player.getIp();
+            playerCache.wasInPortal = player.getBlockStateAtPos().getBlock().equals(Blocks.NETHER_PORTAL);
+
+            // Setting the session expire time
+            if (config.main.sessionTimeoutTime != -1)
+                playerCache.validUntil = System.currentTimeMillis() + config.main.sessionTimeoutTime * 1000L;
+        } else if (config.main.spawnOnJoin) {
+            ((PlayerAuth) player).hidePosition(false);
+
+            player.setInvulnerable(false);
+            player.setInvisible(false);
+        }
+    }
+
+    // Player chatting
+    public static ActionResult onPlayerChat(ServerPlayerEntity player, String message) {
+        // Getting the message to then be able to check it
+        if (
+                !((PlayerAuth) player).isAuthenticated() &&
+                        !message.startsWith("/login") &&
+                        !(message.startsWith("/l") && config.experimental.enableAliases) &&
+                        !message.startsWith("/register") &&
+                        (!config.experimental.allowChat || message.startsWith("/"))
+        ) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return ActionResult.FAIL;
+        }
+        return ActionResult.PASS;
+    }
+
+    // Player movement
+    public static ActionResult onPlayerMove(ServerPlayerEntity player) {
+        // Player will fall if enabled (prevent fly kick)
+        boolean auth = ((PlayerAuth) player).isAuthenticated();
+        // Otherwise, movement should be disabled
+        if (!auth && !config.experimental.allowMovement) {
+            if (System.nanoTime() >= lastAcceptedPacket + config.experimental.teleportationTimeoutInMs * 1000000) {
+                player.networkHandler.requestTeleport(player.getX(), player.getY(), player.getZ(), player.getYaw(), player.getPitch());
+                lastAcceptedPacket = System.nanoTime();
+            }
+            if (!player.isInvulnerable())
+                player.setInvulnerable(true);
+            return ActionResult.FAIL;
+        }
+        return ActionResult.PASS;
+    }
+
+    // Using a block (right-click function)
+    public static ActionResult onUseBlock(PlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.experimental.allowBlockUse) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return ActionResult.FAIL;
+        }
+        return ActionResult.PASS;
+    }
+
+    // Breaking a block
+    public static boolean onBreakBlock(PlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.experimental.allowBlockPunch) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return false;
+        }
+        return true;
+    }
+
+    // Using an item
+    public static TypedActionResult<ItemStack> onUseItem(PlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.experimental.allowItemUse) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return TypedActionResult.fail(ItemStack.EMPTY);
+        }
+
+        return TypedActionResult.pass(ItemStack.EMPTY);
+    }
+
+    // Dropping an item
+    public static ActionResult onDropItem(PlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.experimental.allowItemDrop) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return ActionResult.FAIL;
+        }
+        return ActionResult.PASS;
+    }
+
+    // Changing inventory (item moving etc.)
+    public static ActionResult onTakeItem(ServerPlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.experimental.allowItemMoving) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return ActionResult.FAIL;
+        }
+
+        return ActionResult.PASS;
+    }
+
+    // Attacking an entity
+    public static ActionResult onAttackEntity(PlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.experimental.allowEntityPunch) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return ActionResult.FAIL;
+        }
+
+        return ActionResult.PASS;
+    }
+
+    // Interacting with entity
+    public static ActionResult onUseEntity(PlayerEntity player) {
+        if (!((PlayerAuth) player).isAuthenticated() && !config.main.allowEntityInteract) {
+            player.sendMessage(((PlayerAuth) player).getAuthMessage(), false);
+            return ActionResult.FAIL;
+        }
+
+        return ActionResult.PASS;
+    }
+}
---
+++ LoginCommand.java
@@ -0,0 +1,107 @@
+package xyz.nikitacartes.easyauth.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import me.lucko.fabric.api.permissions.v0.Permissions;
+import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import xyz.nikitacartes.easyauth.utils.AuthHelper;
+import xyz.nikitacartes.easyauth.utils.PlayerAuth;
+import xyz.nikitacartes.easyauth.utils.TranslationHelper;
+
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+import static xyz.nikitacartes.easyauth.EasyAuth.*;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class LoginCommand {
+    
+    // To reset the login attempts...
+    public static final ScheduledExecutorService RESET_LOGIN_THREAD = Executors.newScheduledThreadPool(1);
+    
+    public static void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher) {
+        LiteralCommandNode<ServerCommandSource> node = registerLogin(dispatcher); // Registering the "/login" command
+        if (config.experimental.enableAliases) {
+            dispatcher.register(literal("l")
+                    .requires(Permissions.require("easyauth.commands.login", true))
+                    .redirect(node));
+        }
+    }
+
+    public static LiteralCommandNode<ServerCommandSource> registerLogin(CommandDispatcher<ServerCommandSource> dispatcher) {
+        return dispatcher.register(literal("login")
+                .requires(Permissions.require("easyauth.commands.login", true))
+                .then(argument("password", string())
+                        .executes(ctx -> login(ctx.getSource(), getString(ctx, "password")) // Tries to authenticate user
+                        ))
+                .executes(ctx -> {
+                    ctx.getSource().getPlayer().sendMessage(TranslationHelper.getEnterPassword(), false);
+                    return 0;
+                }));
+    }
+
+    // Method called for checking the password
+    private static int login(ServerCommandSource source, String pass) throws CommandSyntaxException {
+        // Getting the player who send the command
+        ServerPlayerEntity player = source.getPlayer();
+        String uuid = ((PlayerAuth) player).getFakeUuid();
+        if (((PlayerAuth) player).isAuthenticated()) {
+            player.sendMessage(TranslationHelper.getAlreadyAuthenticated(), false);
+            return 0;
+        }
+        
+        // ++ the login tries. Maybe it's more threadsafe here than in the thread pool?
+        // But if not, you could save the calls to set this back to 0.
+        playerCacheMap.get(uuid).loginTries++;
+        
+        // Putting rest of the command in different thread to avoid lag spikes
+        THREADPOOL.submit(() -> {
+        	int maxLoginTries = config.main.maxLoginTries;
+            AuthHelper.PasswordOptions passwordResult = AuthHelper.checkPassword(uuid, pass.toCharArray());
+
+            if (passwordResult == AuthHelper.PasswordOptions.CORRECT) {
+                player.sendMessage(TranslationHelper.getSuccessfullyAuthenticated(), false);
+                ((PlayerAuth) player).setAuthenticated(true);
+                
+                // Reset their login tries
+                playerCacheMap.get(uuid).loginTries = 0;
+                
+                // player.getServer().getPlayerManager().sendToAll(new PlayerListS2CPacket(PlayerListS2CPacket.Action.ADD_PLAYER, player));
+                return;
+            } else if (passwordResult == AuthHelper.PasswordOptions.NOT_REGISTERED) {
+                player.sendMessage(TranslationHelper.getRegisterRequired(), false);
+                
+            	// Reset their login tries
+                playerCacheMap.get(uuid).loginTries = 0;
+                
+                return;
+            }
+            // Kicking the player out
+            else if (maxLoginTries == 1) {
+            	// Reset their login tries
+                playerCacheMap.get(uuid).loginTries = 0;
+                
+                player.networkHandler.disconnect(TranslationHelper.getWrongPassword());
+                return;
+            } else if(playerCacheMap.get(uuid).loginTries == maxLoginTries) {
+            	player.networkHandler.disconnect(TranslationHelper.getLoginTriesExceeded());
+            	
+            	// Reset their login try counter after the amount of seconds specified in the config.
+            	RESET_LOGIN_THREAD.schedule(() -> {
+            		playerCacheMap.get(uuid).loginTries = 0;
+            	}, config.experimental.resetLoginAttemptsTime, TimeUnit.SECONDS);
+                return;
+            }
+            // Sending wrong pass message
+            player.sendMessage(TranslationHelper.getWrongPassword(), false);
+        });
+        return 0;
+    }
+}

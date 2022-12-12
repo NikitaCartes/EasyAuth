089f638ddf6e553a108bf835ac4233f2c75dcb10
NikitaCartes <nikich98@yandex.ru>
2022-12-13 00:20:57 +0300

    Revert changes for 1.19

--- src/main/java/xyz/nikitacartes/easyauth/mixin/CommandManagerMixin.java
+++ src/main/java/xyz/nikitacartes/easyauth/mixin/CommandManagerMixin.java
@@ -1,6 +1,5 @@
 package xyz.nikitacartes.easyauth.mixin;
 
-import com.mojang.brigadier.ParseResults;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.ActionResult;
@@ -12,9 +11,9 @@ import xyz.nikitacartes.easyauth.event.AuthEventHandler;
 
 @Mixin(CommandManager.class)
 public class CommandManagerMixin {
-    @Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;Ljava/lang/String;)I", at = @At("HEAD"), cancellable = true)
-    private void checkCanUseCommands(ParseResults<ServerCommandSource> parseResults, String command, CallbackInfoReturnable<Integer> cir) {
-        ActionResult result = AuthEventHandler.onPlayerCommand(parseResults.getContext().getSource().getPlayer(), command);
+    @Inject(method = "execute(Lnet/minecraft/server/command/ServerCommandSource;Ljava/lang/String;)I", at = @At("HEAD"), cancellable = true)
+    private void checkCanUseCommands(ServerCommandSource source, String command, CallbackInfoReturnable<Integer> cir) {
+        ActionResult result = AuthEventHandler.onPlayerCommand(source.getPlayer(), command);
         if (result == ActionResult.FAIL) {
             cir.setReturnValue(1);
         }

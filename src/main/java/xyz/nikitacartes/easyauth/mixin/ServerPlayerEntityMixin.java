11609a73df579e33288e586d3572523cb4db722b
NikitaCartes <nikich98@yandex.ru>
2023-06-11 22:03:53 +0200

    Backport to 1.19.3

--- src/main/java/xyz/nikitacartes/easyauth/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/xyz/nikitacartes/easyauth/mixin/ServerPlayerEntityMixin.java
@@ -257,7 +257,7 @@ public abstract class ServerPlayerEntityMixin implements PlayerAuth {
     private void playerTick(CallbackInfo ci) {
         if (!this.easyAuth$isAuthenticated()) {
             // Checking player timer
-            if (kickTimer <= 0 && player.networkHandler.isConnectionOpen()) {
+            if (kickTimer <= 0 && player.networkHandler.getConnection().isOpen()) {
                 player.networkHandler.disconnect(langConfig.timeExpired.get());
             } else {
                 // Sending authentication prompt every 10 seconds

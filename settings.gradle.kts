06cec5d8eb479fcabfc32282c1642fc42588cd91
NikitaCartes <nikich98@yandex.ru>
2026-06-15 02:29:19 +0200

    Remove 1.21.4 as separate version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ stonecutter {
     create(rootProject) {
 
         // Obfuscated Fabric
-        listOf("1.19.4", "1.20", "1.20.2", "1.20.3", "1.20.5", "1.21", "1.21.2", "1.21.4", "1.21.5", "1.21.6", "1.21.9", "1.21.11").forEach { mc ->
+        listOf("1.19.4", "1.20", "1.20.2", "1.20.3", "1.20.5", "1.21", "1.21.2", "1.21.5", "1.21.6", "1.21.9", "1.21.11").forEach { mc ->
             versions("$mc-fabric" to mc).buildscript("build.fabric-obf.gradle.kts")
         }
         // Deobfuscated Fabric
@@ -25,7 +25,7 @@ stonecutter {
             versions("$mc-fabric" to mc).buildscript("build.fabric-deobf.gradle.kts")
         }
         // NeoForge
-        listOf("1.21", "1.21.2", "1.21.4", "1.21.5", "1.21.6", "1.21.9", "1.21.11", "26.1").forEach { mc ->
+        listOf("1.21", "1.21.2", "1.21.5", "1.21.6", "1.21.9", "1.21.11", "26.1").forEach { mc ->
             versions("$mc-neoforge" to mc).buildscript("build.neoforge.gradle.kts")
         }
         vcsVersion = "1.21.11-fabric"

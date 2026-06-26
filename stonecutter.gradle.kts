5860d3b1d670c1da501093df7151607ef361a912
NikitaCartes <nikich98@yandex.ru>
2026-06-27 00:06:44 +0200

    fixup! Simplify stonecutter

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -7,8 +7,7 @@ stonecutter active "26.2-fabric"
 stonecutter parameters {
     constants.match(current.project.substringAfterLast('-'), "fabric", "neoforge")
 
-    val deobfFabric = current.project.endsWith("-fabric") && current.parsed >= "26"
-    replacements.string(deobfFabric, "resource_location") {
+    replacements.string(current.parsed >= "1.21.11", "resource_location") {
         replace("ResourceLocation", "Identifier")
         replace("location()", "identifier()")
     }

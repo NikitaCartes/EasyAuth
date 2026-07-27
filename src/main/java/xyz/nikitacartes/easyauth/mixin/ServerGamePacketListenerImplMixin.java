6da5cb2ee0415ad39e39ba5d05d6eab5bcf454d4
NikitaCartes <nikich98@yandex.ru>
2026-07-28 00:36:32 +0200

    Fix #278, endless vanish

--- src/main/java/xyz/nikitacartes/easyauth/mixin/ServerGamePacketListenerImplMixin.java
+++ src/main/java/xyz/nikitacartes/easyauth/mixin/ServerGamePacketListenerImplMixin.java
@@ -2,6 +2,7 @@ package xyz.nikitacartes.easyauth.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.network.DisconnectionDetails;
 import net.minecraft.network.protocol.game.ServerboundChatPacket;
 import net.minecraft.network.protocol.game.ServerboundChatSessionUpdatePacket;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
@@ -11,6 +12,7 @@ import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import xyz.nikitacartes.easyauth.event.AuthEventHandler;
+import xyz.nikitacartes.easyauth.integrations.VanishIntegration;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.nikitacartes.easyauth.interfaces.PlayerAuth;
 
 import static net.minecraft.network.protocol.game.ServerboundPlayerActionPacket.Action.SWAP_ITEM_WITH_OFFHAND;
+import static xyz.nikitacartes.easyauth.EasyAuth.config;
 import static xyz.nikitacartes.easyauth.EasyAuth.extendedConfig;
 
 @Mixin(ServerGamePacketListenerImpl.class)
@@ -102,6 +105,17 @@ public abstract class ServerGamePacketListenerImplMixin {
     private void onPlayerSessionWrap(ServerboundChatSessionUpdatePacket packet, Operation<Void> original) {
         if (!extendedConfig.forcedOfflineUuid || !((PlayerAuth) this.player).easyAuth$isUsingMojangAccount()) {
             original.call(packet);
+        }
+    }
+
+    @Inject(method = "onDisconnect(Lnet/minecraft/network/DisconnectionDetails;)V", at = @At("TAIL"))
+    private void onPlayerDisconnectUnVanish(DisconnectionDetails details, CallbackInfo ci) {
+        PlayerAuth playerAuth = (PlayerAuth) this.player;
+        if (playerAuth.easyAuth$canSkipAuth() || playerAuth.easyAuth$isAuthenticated()) {
+            return;
+        }
+        if (config.vanishUntilAuth) {
+            VanishIntegration.setVanished(this.player, playerAuth.easyAuth$wasVanished());
         }
     }
 }
